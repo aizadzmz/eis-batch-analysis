9ead3c201057318541235aa893ef4d72dd95fc64
Aizad <aizadzmz@gmail.com>
2026-07-15 13:45:31 +0100

    Revert "mb + eis parser, first batch GUI, library update"
    This reverts commit 3b931d5f89830afc406cd92cec94d7c39fe4254b.

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,10 @@
-from core.io_utils import EISDataset, EISParseError, parse_eis_file
+from core.io_utils import EISDataset, EISParseError, parse_mpt
 from core.plotting import plot_single, plot_overlay
 
 __all__ = [
     "EISDataset",
     "EISParseError",
-    "parse_eis_file",
+    "parse_mpt",
     "plot_single",
     "plot_overlay",
 ]
